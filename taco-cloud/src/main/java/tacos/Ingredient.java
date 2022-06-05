cf7abf5b223217d16760fa4cf26d521971f0d215
Пользователь <mniti-66@mail.ru>
2022-06-05 23:13:04 +0300

    Part 1 Chapter 2 (before validation). DesignTacoController and OrderController were added with html pages. File ctyles.css

---
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -0,0 +1,17 @@
+package tacos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
