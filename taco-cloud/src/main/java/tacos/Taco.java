fd2cd38bb4e18f5013667d38d9dc2168728fb0f0
Пользователь <mniti-66@mail.ru>
2022-06-09 00:25:51 +0300

    Part 1 Chapter 2 (validation done). Chapter 3 (persistence for taco cloud added using JdbcTemplate)

--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -1,12 +1,25 @@
 package tacos;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
+//TODO ошибки при вводе недостаточного количества ингридиетнов не обрабатываются в design.html.
 @Data
 public class Taco {
 
+    private Long id;
+
+    private Date createdAt;
+
+    @NotNull
+    @Size(min=5, message = "Name must be at least 5 characters long")
     private String name;
-    private List<String> Ingredient;
+
+    @Size(min=1, message = "You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients;
 }
